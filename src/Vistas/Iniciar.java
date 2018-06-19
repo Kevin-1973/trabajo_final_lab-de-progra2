25e34dc10c30ba61b95303aa4241b491eac7ede4
Kevin Humberto Cabeza Ruiz <kevinruiz1973@gmail.com>
2018-06-19 00:40:45 -0600

    inicio

---
+++ src/Vistas/Iniciar.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vistas;
+
+/**
+ *
+ * @author Alexis Gardez
+ */
+public class Iniciar {
+
+    public static void main(String[] args) {
+        ProgressBar p = new ProgressBar();
+        p.show();
+
+    }
+
+}
